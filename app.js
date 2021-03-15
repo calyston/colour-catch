22598d5ffe5cdb75047f1292f4c12379b405f606
Christine Lyston <calyston.14@gmail.com>
2021-03-15 15:53:35 +0000

    feat: hex generator created

---
+++ app.js
@@ -0,0 +1,15 @@
+//Global Selections and Variables
+
+
+//Functions
+
+function generateHex() {
+  const letters = '0123456789ABCDEF';
+  let hash = '#';
+  for (let i = 0; i < 6; i++) {
+    hash += letters[Math.floor(Math.random() * 16)];
+  }
+  return hash;
+}
+let randomHex = generateHex();
+console.log(randomHex);
